b3190791a25195f04a0c54499517f0e354e1a809
Miska23 <miskalinden@gmail.com>
2020-11-29 21:08:29 +0200

    exercise 7.9 done

---
+++ bloglist/bloglist-frontend/src/reducers/notificationReducer.js
@@ -0,0 +1,38 @@
+const reducer = (state = null, action) => {
+  switch (action.type) {
+  case 'SET_NOTIFICATION':
+    return action
+  case 'CLEAR_NOTIFICATION':
+    return null
+  default:
+    return state
+  }
+}
+
+let timeoutId
+
+export const setNotification = (content, time, messageType) => {
+  return async dispatch => {
+    dispatch({
+      type: 'SET_NOTIFICATION',
+      content,
+      messageType
+    })
+
+    if (timeoutId) {
+      clearTimeout(timeoutId)
+    }
+
+    timeoutId = setTimeout(() => {
+      dispatch({
+        type: 'CLEAR_NOTIFICATION'
+      })
+    }, time * 1000)
+  }
+}
+
+export const clearNotification = () => (
+  { type: 'CLEAR_NOTIFICATION' }
+)
+
+export default reducer
